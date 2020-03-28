aebb5990171899b5d7c14b84819330a4d8b9abda
David Zhang <davidzhang312@gmail.com>
2020-03-27 21:57:12 -0400

    added csv file

--- matching.py
+++ matching.py
@@ -6,7 +6,7 @@ with open("PatientMatchingData.csv") as infile:
 
 groups = {}
 for line in data:
-    lname = line[5].lower()
+    lname = line[5].lower().strip()
     if lname in groups.keys():
         groups[lname].append((line[0],line[1]))
     else:
