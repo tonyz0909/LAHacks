22b8c146a5d33adb32f1c487acf96c376c186b2e
David Zhang <davidzhang312@gmail.com>
2020-03-27 21:52:33 -0400

    new file

---
+++ matching.py
@@ -0,0 +1,16 @@
+import csv
+with open("PatientMatchingData.csv") as infile:
+    reader = csv.reader(infile)
+    reader = list(reader)
+    data = reader[1:]
+
+groups = {}
+for line in data:
+    lname = line[5].lower()
+    if lname in groups.keys():
+        groups[lname].append((line[0],line[1]))
+    else:
+        groups[lname] = [(line[0],line[1])]
+
+print(len(groups.keys()))   
+
